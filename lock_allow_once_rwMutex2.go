b39896b44f6f4af612bc70f8df13a68c183bddb3
Dav He <s991179@gmail.com>
2024-09-06 10:20:35 +0800

    fix: use RLock

--- lock_allow_once_rwMutex2.go
+++ lock_allow_once_rwMutex2.go
@@ -14,26 +14,26 @@ func Lock_Rw_Test2() {
 	wg.Add(2)
 	go func() {
 		defer wg.Done()
-		lock.Lock()
+		lock.RLock()
 		for i := 0; i < 10; i++ {
 			val++
 			time.Sleep(1 * time.Second)
 		}
-		lock.Unlock()
+		lock.RUnlock()
 	}()
 
 	go func() {
-		time.Sleep(10 * time.Millisecond)
+		time.Sleep(1 * time.Second)
 		defer wg.Done()
-		lock.Lock()
+		lock.RLock()
 		for i := 0; i < 10; i++ {
 			fmt.Println("i:", val)
 		}
 		val++
 		fmt.Println("after:", val)
-		lock.Unlock()
+		lock.RUnlock()
 	}()
 
 	wg.Wait()
-	fmt.Println("val:", val)
+	fmt.Println("final val:", val)
 }
