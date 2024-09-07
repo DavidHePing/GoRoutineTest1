3fef3b3e55f67b739d5fe1340f5e67e550dbdbf2
Dav He <s991179@gmail.com>
2024-09-08 07:05:31 +0800

    feat: test r lock

--- lock_allow_once_rwMutex1.go
+++ lock_allow_once_rwMutex1.go
@@ -10,22 +10,22 @@ func Lock_Rw_Test1() {
 	var val1 int32
 	var wg sync.WaitGroup
 	var lock sync.RWMutex
+	wg.Add(1000)
 	for i := 0; i < 1000; i++ {
-		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			lock.Lock()
+			lock.RLock()
 			val1++
-			lock.Unlock()
+			lock.RUnlock()
 		}()
 	}
 	wg.Wait()
-	fmt.Println("val1:", atomic.LoadInt32(&val1)) //must be 1000
+	fmt.Println("val1:", atomic.LoadInt32(&val1)) //may be not 1000
 
 	var val2 int32
 	var wg2 sync.WaitGroup
+	wg2.Add(1000)
 	for i := 0; i < 1000; i++ {
-		wg2.Add(1)
 		go func() {
 			defer wg2.Done()
 			val2++
